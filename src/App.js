6c2bc9c34c78ecaa0b58c026c3b3a31716f135f6
Alexander Haardt <haardt.alexander@gmail.com>
2019-02-28 14:55:30 -0500

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+const store = ConfigureStore();
 
 class App extends Component {
 
 
+
   render() {
     return (
-        //Enclosed in BrowserRouter to make use of React Navigation links
+      //Enclosed in BrowserRouter to make use of React Navigation links
+      //Enclose in provider for Redux
+        <Provider store = {store}>
         <BrowserRouter>
           <div className="App">
           <Main />
           </div>
         </BrowserRouter>
+        </Provider>
     );
   }
 }
